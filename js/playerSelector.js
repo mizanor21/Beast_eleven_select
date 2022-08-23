e37ca57c67d98a97423bb6041b5f8f11403fa020
mizanor21 <mizanorrahman398@gmail.com>
2022-08-23 06:54:49 +0600

    Player List added in JavaScript

---
+++ js/playerSelector.js
@@ -0,0 +1,108 @@
+// Player List start 
+document.getElementById('btn_afif').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Afif Hossain';
+    playerList.appendChild(li);
+    document.getElementById('btn_afif').setAttribute("disabled", "");
+})
+document.getElementById('btn_shakib').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Shakib Al Hasan (C)';
+    playerList.appendChild(li);
+    document.getElementById('btn_shakib').setAttribute("disabled", "");
+})
+document.getElementById('btn_anamul').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Anamul Haque';
+    playerList.appendChild(li);
+    document.getElementById('btn_anamul').setAttribute("disabled", "");
+})
+document.getElementById('btn_ebadat').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Ebadot Hossain';
+    playerList.appendChild(li);
+    document.getElementById('btn_ebadat').setAttribute("disabled", "");
+})
+document.getElementById('btn_fizz').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Mustafizur Rahman';
+    playerList.appendChild(li);
+    document.getElementById('btn_fizz').setAttribute("disabled", "");
+})
+document.getElementById('btn_mahmudullah').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Mahmudullah';
+    playerList.appendChild(li);
+    document.getElementById('btn_mahmudullah').setAttribute("disabled", "");
+})
+document.getElementById('btn_miraz').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Mehidy Hasan Miraz';
+    playerList.appendChild(li);
+    document.getElementById('btn_miraz').setAttribute("disabled", "");
+})
+document.getElementById('btn_mosaddek').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Mosaddek Hossain';
+    playerList.appendChild(li);
+    document.getElementById('btn_mosaddek').setAttribute("disabled", "");
+})
+document.getElementById('btn_musfiq').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Mushfiqur Rahim (W)';
+    playerList.appendChild(li);
+    document.getElementById('btn_musfiq').setAttribute("disabled", "");
+})
+document.getElementById('btn_nasum').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Nasum Ahmed';
+    playerList.appendChild(li);
+    document.getElementById('btn_nasum').setAttribute("disabled", "");
+})
+document.getElementById('btn_nurul').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Nurul hasan';
+    playerList.appendChild(li);
+    document.getElementById('btn_nurul').setAttribute("disabled", "");
+})
+document.getElementById('btn_sabbir').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Sabbir Rahman';
+    playerList.appendChild(li);
+    document.getElementById('btn_sabbir').setAttribute("disabled", "");
+})
+document.getElementById('btn_saif').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Saifuddin';
+    playerList.appendChild(li);
+    document.getElementById('btn_saif').setAttribute("disabled", "");
+})
+document.getElementById('btn_mahedi').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Mahedi Hasan';
+    playerList.appendChild(li);
+    document.getElementById('btn_mahedi').setAttribute("disabled", "");
+})
+
+document.getElementById('btn_taskin').addEventListener('click', function () {
+    const playerList = document.getElementById('playerList');
+    const li = document.createElement('li');
+    li.innerText = 'Taskin Ahmed';
+    playerList.appendChild(li);
+    document.getElementById('btn_taskin').setAttribute("disabled", "");
+})
+// Player List End 
