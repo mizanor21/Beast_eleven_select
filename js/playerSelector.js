999dc6c093d4dcc222a086d84b4b338dcde91a75
mizanor21 <mizanorrahman398@gmail.com>
2022-08-23 23:25:30 +0600

    js file some edited

--- js/playerSelector.js
+++ js/playerSelector.js
@@ -3,6 +3,7 @@ document.getElementById('btn_afif').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Afif Hossain';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_afif').setAttribute("disabled", "");
 })
@@ -10,6 +11,7 @@ document.getElementById('btn_shakib').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Shakib Al Hasan (C)';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_shakib').setAttribute("disabled", "");
 })
@@ -17,6 +19,7 @@ document.getElementById('btn_anamul').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Anamul Haque';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_anamul').setAttribute("disabled", "");
 })
@@ -24,6 +27,7 @@ document.getElementById('btn_ebadat').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Ebadot Hossain';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_ebadat').setAttribute("disabled", "");
 })
@@ -31,6 +35,7 @@ document.getElementById('btn_fizz').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Mustafizur Rahman';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_fizz').setAttribute("disabled", "");
 })
@@ -38,6 +43,7 @@ document.getElementById('btn_mahmudullah').addEventListener('click', function ()
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Mahmudullah';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_mahmudullah').setAttribute("disabled", "");
 })
@@ -45,6 +51,7 @@ document.getElementById('btn_miraz').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Mehidy Hasan Miraz';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_miraz').setAttribute("disabled", "");
 })
@@ -52,6 +59,7 @@ document.getElementById('btn_mosaddek').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Mosaddek Hossain';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_mosaddek').setAttribute("disabled", "");
 })
@@ -59,6 +67,7 @@ document.getElementById('btn_musfiq').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Mushfiqur Rahim (W)';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_musfiq').setAttribute("disabled", "");
 })
@@ -66,6 +75,7 @@ document.getElementById('btn_nasum').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Nasum Ahmed';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_nasum').setAttribute("disabled", "");
 })
@@ -73,6 +83,7 @@ document.getElementById('btn_nurul').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Nurul hasan';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_nurul').setAttribute("disabled", "");
 })
@@ -80,6 +91,7 @@ document.getElementById('btn_sabbir').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Sabbir Rahman';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_sabbir').setAttribute("disabled", "");
 })
@@ -87,6 +99,7 @@ document.getElementById('btn_saif').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Saifuddin';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_saif').setAttribute("disabled", "");
 })
@@ -94,6 +107,7 @@ document.getElementById('btn_mahedi').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Mahedi Hasan';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_mahedi').setAttribute("disabled", "");
 })
@@ -102,26 +116,25 @@ document.getElementById('btn_taskin').addEventListener('click', function () {
     const playerList = document.getElementById('playerList');
     const li = document.createElement('li');
     li.innerText = 'Taskin Ahmed';
+    li.classList.add('item');
     playerList.appendChild(li);
     document.getElementById('btn_taskin').setAttribute("disabled", "");
 })
 // Player List End
-
-// Count Player Added
-function countItems(playerList) {
-    var ol = document.getElementById(playerList);
-    var i = 0, itemCount = 0;
-    while (ol.getElementsByTagName('li')[i++]) itemCount++;
-    document.write(itemCount);
-
-}
 
 // Players Expenses area 
 const txt = document.getElementById('inputFild');
 const out = document.getElementById('p_expenses');
 
 document.getElementById('btn_total_expense').addEventListener('click', function () {
-    out.innerHTML = txt.value * 11;
+    const playerList = document.querySelectorAll('li');
+    const players = playerList.length;
+    if (players < 12) {
+        out.innerHTML = txt.value * players;
+    }
+    else {
+        alert("Please Select Best XI or Less!");
+    }
 })
 
 //Total cost area 
@@ -129,7 +142,9 @@ document.getElementById('btn_total_expense').addEventListener('click', function 
 document.getElementById('btn_total_cost').addEventListener('click', function () {
     const playerField = document.getElementById('inputFild');
     const perPlayerAmount = parseFloat(playerField.value);
-    const playersAmount = perPlayerAmount * 11;
+    const playerList = document.querySelectorAll('li');
+    const players = playerList.length;
+    const playersAmount = perPlayerAmount * players;
 
     const managerField = document.getElementById('managerInputFild');
     const managerAmount = parseFloat(managerField.value);
@@ -139,8 +154,5 @@ document.getElementById('btn_total_cost').addEventListener('click', function () 
     const totalElement = document.getElementById('t_expenses');
     const total = playersAmount + managerAmount + coachAmount;
     totalElement.innerText = total;
-
-    // managerField.value = '';
-    // coachField.value = '';
 
 })
